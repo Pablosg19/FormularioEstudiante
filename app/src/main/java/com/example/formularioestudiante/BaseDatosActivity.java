c9c4babd6c03083d161c867428b86a68dcb24bd7
Pablosg19 <pablosg191001@gmail.com>
2022-11-04 16:54:20 +0100

    Formulario Estudiante finalizado

--- app/src/main/java/com/example/formularioestudiante/BaseDatosActivity.java
+++ app/src/main/java/com/example/formularioestudiante/BaseDatosActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.formularioestudiante.clases.Estudiante;
 
 public class BaseDatosActivity extends AppCompatActivity {
+
+    private Estudiante e = null;
+    private TextView txtDni = null;
+    private TextView txtNombre = null;
+    private TextView txtCurso = null;
+    private TextView txtFecha = null;
+    private TextView txtHora = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +26,20 @@ public class BaseDatosActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if(intent != null){
-            Estudiante e =(Estudiante) intent.getSerializableExtra(MainActivity.EXTRA_ESTUDIANTE);
-            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+             e =(Estudiante) intent.getSerializableExtra(MainActivity.EXTRA_ESTUDIANTE);
+        }
+        if (e != null){
+            txtDni = (TextView) findViewById(R.id.txt_dni);
+            txtDni.setText("DNI: \n" + e.getDni());
+            txtNombre = (TextView) findViewById(R.id.txt_nombre);
+            txtNombre.setText("Nombre: \n" + e.getNombre());
+            txtCurso = (TextView) findViewById(R.id.txt_curso);
+            txtCurso.setText("Curso: \n" + e.getCurso());
+            txtFecha = (TextView) findViewById(R.id.txt_fechaNacimiento);
+            txtFecha.setText("Fecha: \n" + e.getFecha_nacimiento());
+            txtHora = (TextView) findViewById(R.id.txt_horaVisita);
+            txtHora.setText("Hora: \n" + e.getHora_tutoria());
+
         }
     }
 }
